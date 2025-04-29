477aed959fd785990752e5664f7168b546179254
ShizukuNanami <129707817+Shizuku-Nanami@users.noreply.github.com>
2025-04-29 10:53:57 +0800

    add

--- config.ts
+++ config.ts
@@ -1,22 +1,24 @@
 export const opensource = {
     active: [
-        'chitsanfei/rin-asf-bot',
-        'chitsanfei/monster-medical-learning-book',
-      //'',
+        'Hikarisame-Technology/SYSTools',
+        'Hikarisame-Technology/ADBTools',
+        'Hikarisame-Technology/ACGManager',
+        'Hikarisame-Technology/Suspend-Classroom-System',
+        'Forever331/Dell_G5_5500_Hackintosh_OpenCore',
+        // '',
     ],
     toys: {
       limit: 3,
       random: true,
       repos: [
-        'chitsanfei/koishi-plugin-setu',
-        'chitsanfei/sekai-voice-fetch', 
+        'Shizuku-Nanami/Minigeki',
         //'',
       ],
     },
   }
   export const motto = `<p align=center><strong><i> ちきゅう大爆発 どっか～ん  </i></strong></p>`
   export const github = {
-    name: 'chitsanfei',
+    name: 'Shizuku-Nanami',
   }
 
   export const mxSpace = {
